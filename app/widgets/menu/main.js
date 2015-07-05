157e7bd8243e17a4e490369e73f02dfe16d82475
faceach <faceach@gmail.com>
2015-07-05 12:38:07 +0800

    menu selector

--- app/widgets/menu/main.js
+++ app/widgets/menu/main.js
@@ -6,6 +6,10 @@ angular.module('mates.menu', [])
     '$scope',
     function($scope) {
 
+        $scope.photo = {
+            "src": "./img/blank.png"
+        };
+
         $scope.levelSelector = {
             "visible": false
         };
@@ -18,5 +22,56 @@ angular.module('mates.menu', [])
             console.log("Selected level: " + levelId)
         }
 
+        $scope.addPhoto = function($files) {
+            console.log("files exist");
+
+            if (!$files || $files.length <= 0) {
+                return;
+            }
+            // Loop through the FileList and render image files as thumbnails.
+            var f = $files[0];
+
+            // Only process image files.
+            // f.type === "" under Wechat (device: Samsung Android v4.4.x)
+            if (f.type && !f.type.match('image.*')) {
+                return;
+            }
+
+            (new ImagePinch({
+                file: f,
+                toHeight: 400, // *px
+                maxSize: 512, // *kb
+                success: function(file) {
+                    var reader = new FileReader();
+                    // Closure to capture the file information.
+                    reader.onload = function(e) {
+                        var theFile = $scope.photo.src = e.target.result;
+
+                        // TODO: do not call $apply()
+                        if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
+                            $scope.$apply();
+                        }
+
+                        // Render thumbnail.
+                        /*loadImage.parseMetaData(theFile, function(data) {
+                            var rotation = 0;
+                            if (data && data.exif) {
+                                var orientation = data.exif.get('Orientation');
+                                if (orientation === 8) {
+                                    rotation = 90;
+                                } else if (orientation === 3) {
+                                    rotation = 180;
+                                } else if (orientation === 6) {
+                                    rotation = 270;
+                                }
+                            }
+                            processRequest(true, theFile, null, theFile.size, rotation);
+                        });*/
+                    };
+                    // Read in the image file as a data URL.
+                    reader.readAsDataURL(file);
+                }
+            })).pinch();
+        };
     }
 ]);
--- app/widgets/photo/main.js
+++ app/widgets/photo/main.js
@@ -127,59 +127,6 @@ angular.module('mates.photo', [])
             });
             face && (face.edit = true);
         };
-
-        $scope.onFileSelect = function($files) {
-            console.log("files exist");
-
-            if (!$files || $files.length <= 0) {
-                return;
-            }
-            // Loop through the FileList and render image files as thumbnails.
-            var f = $files[0];
-
-            // Only process image files.
-            // f.type === "" under Wechat (device: Samsung Android v4.4.x)
-            if (f.type && !f.type.match('image.*')) {
-                return;
-            }
-
-            (new ImagePinch({
-                file: f,
-                toHeight: 400, // *px
-                maxSize: 512, // *kb
-                success: function(file) {
-                    var reader = new FileReader();
-                    // Closure to capture the file information.
-                    reader.onload = function(e) {
-                        var theFile = $scope.photo.src = e.target.result;
-
-                        // TODO: do not call $apply()
-                        if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') {
-                            $scope.$apply();
-                        }
-
-                        // Render thumbnail.
-                        /*loadImage.parseMetaData(theFile, function(data) {
-                            var rotation = 0;
-                            if (data && data.exif) {
-                                var orientation = data.exif.get('Orientation');
-                                if (orientation === 8) {
-                                    rotation = 90;
-                                } else if (orientation === 3) {
-                                    rotation = 180;
-                                } else if (orientation === 6) {
-                                    rotation = 270;
-                                }
-                            }
-                            processRequest(true, theFile, null, theFile.size, rotation);
-                        });*/
-                    };
-                    // Read in the image file as a data URL.
-                    reader.readAsDataURL(file);
-                }
-            })).pinch();
-        };
-
     }
 ])
 
